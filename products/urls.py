13fc6b5fe4f3b5dd31957189e8c6993601e5d75a
Dania2021 <>
2023-05-13 00:46:38 +0000

    Added product details functionality #7

--- products/urls.py
+++ products/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
         views.all_products,
         name='products'
     ),
+    path(
+        '<int:product_id>/',
+        views.product_detail,
+        name='product_detail'),
 ]
