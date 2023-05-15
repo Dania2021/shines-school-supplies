3becd5f4a5ab1d3492f7d5367d61f8da87f60912
Dania2021 <>
2023-05-15 18:06:01 +0000

    Added adjust bag views and url #16

--- bag/urls.py
+++ bag/urls.py
@@ -6,10 +6,16 @@ urlpatterns = [
     path(
         '',
         views.view_bag,
-        name='view_bag'),
+        name='view_bag'
+    ),
     path(
         'add/<item_id>/',
         views.add_to_bag,
         name='add_to_bag'
+    ),
+    path(
+        'adjust/<item_id>/',
+        views.adjust_bag,
+        name='adjust_bag'
     ),
 ]
