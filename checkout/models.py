0ca0f4a3e89d48f17fc518f053a9cafaed44231c
Dania2021 <>
2023-05-16 22:39:11 +0000

    Updated Order class in checkout app

--- checkout/models.py
+++ checkout/models.py
@@ -14,9 +14,10 @@ class Order(models.Model):
     phone_number = models.CharField(max_length=20, null=False, blank=False)
     country = models.CharField(max_length=40, null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
-    city = models.CharField(max_length=40, null=False, blank=False)
-    street_address = models.CharField(max_length=80, null=False, blank=False)
-    state = models.CharField(max_length=80, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=False)
+    county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2,
                                         null=False, default=0)
@@ -24,7 +25,6 @@ class Order(models.Model):
                                       null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2,
                                       null=False, default=0)
-    original_bag = models.TextField(null=False, blank=False, default='')
 
     def _generate_order_number(self):
         """
