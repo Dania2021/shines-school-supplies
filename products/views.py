edb88da5a24852aa170cd4801c7ae0bea54a5010
Dania2021 <>
2023-05-23 22:53:22 +0000

    Finished add product functionality #21

--- products/views.py
+++ products/views.py
@@ -27,7 +27,7 @@ def all_products(request):
                 products = products.annotate(lower_name=Lower('name'))
 
             if sortkey == 'category':
-                sortkey = 'category__name' 
+                sortkey = 'category__name'
 
             if 'direction' in request.GET:
                 direction = request.GET['direction']
@@ -79,10 +79,21 @@ def product_detail(request, product_id):
 
 def add_product(request):
     """ Add a product to the store """
-    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(
+                request, 'Failed to add product.\
+                    Please ensure the form is valid.')
+    else:
+        form = ProductForm()
     template = 'products/add_product.html'
     context = {
         'form': form,
     }
 
-    return render(request, template, context)   
+    return render(request, template, context)
