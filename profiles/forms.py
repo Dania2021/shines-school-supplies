27bec3b336fea3ae7bf9b28071baf14819e2b66c
Dania2021 <>
2023-05-23 15:48:00 +0000

    Product Admin- product form

--- profiles/forms.py
+++ profiles/forms.py
@@ -31,5 +31,5 @@ class UserProfileForm(forms.ModelForm):
                 else:
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'profile-form-input'
+            self.fields[field].widget.attrs['class'] = 'border-black'
             self.fields[field].label = False
