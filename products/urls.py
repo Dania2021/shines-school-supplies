ab16ce3102bd4f4fc5e4350aa48654e0d89abe1b
Dania2021 <>
2023-05-24 10:54:27 +0000

    Product Admin- editing products #22

--- products/urls.py
+++ products/urls.py
@@ -17,4 +17,9 @@ urlpatterns = [
         views.add_product,
         name='add_product'
     ),
+    path(
+        'edit/<int:product_id>/',
+        views.edit_product,
+        name='edit_product'
+    ),
 ]
