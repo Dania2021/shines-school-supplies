0d0a0095b6480d7198e08962d2bab1f134cc3a14
Dania2021 <>
2023-05-29 01:47:02 +0000

    Added 500 eror page, views and url

--- shines_schoolsupplies/urls.py
+++ shines_schoolsupplies/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import handler404
+from .views import handler404, handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -33,3 +33,4 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'shines_schoolsupplies.views.handler404'
+handler500 = 'shines_schoolsupplies.views.handler500'
